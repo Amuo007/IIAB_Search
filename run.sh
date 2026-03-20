a84ee212900719155d14c079c8047a3657e85a34
Amuo <amrinderbalhar@gmail.com>
2026-03-19 20:06:58 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/data/data/com.termux/files/usr/bin/bash
-
+ 
+echo ""
+echo " ___ ___    _    ____    ____                      _     "
+echo "|_ _|_ _|  / \  | __ )  / ___|  ___  __ _ _ __ ___| |__  "
+echo " | | | |  / _ \ |  _ \  \___ \ / _ \/ _\` | '__/ __| '_ \ "
+echo " | | | | / ___ \| |_) |  ___) |  __/ (_| | | | (__| | | |"
+echo "|___|___/_/   \_\____/  |____/ \___|\__,_|_|  \___|_| |_|"
+echo ""
 # Start ollama if not already running
 if ! pgrep -x "ollama" > /dev/null; then
   echo "Starting Ollama..."
